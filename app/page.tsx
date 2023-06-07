12b7ab5b2e5daa859273886ed95dec488ea987c5
nicholasmoreland <nmoreland18@outlook.com>
2023-06-06 23:31:13 -0500

    update: homepage text

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl sm:text-center">
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              Home Page
+              Pinnacle of Style Hairstyling & Barber Studio
             </h2>
             <p className="mt-6 text-lg leading-8 text-gray-600">
               Distinctio et nulla eum soluta et neque labore quibusdam. Saepe et
