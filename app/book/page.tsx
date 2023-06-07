a92d3c3861ee34c115a4ca5623dadd97aed3de6d
nicholasmoreland <nmoreland18@outlook.com>
2023-06-07 02:15:57 -0500

    update: added category & filters

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
 import { NavBar } from "../navbar/navbar";
 import { CheckIcon } from "@heroicons/react/20/solid";
 
@@ -134,7 +137,11 @@ const services = [
   },
 ];
 
+const categories = ["All", "Men", "Women", "Children"];
+
 export default function Home() {
+  const [selectedCategory, setSelectedCategory] = useState("Children");
+
   return (
     <div className="bg-white min-h-screen">
       <NavBar />
@@ -148,71 +155,111 @@ export default function Home() {
             Distinctio et nulla eum soluta et neque labore quibusdam. Saepe et
             quasi iusto modi velit ut non voluptas in. Explicabo id ut laborum.
           </p>
+
+          <div className="mt-6">
+            <div className="flex flex-wrap justify-center items-center text-center space-x-2">
+              <p className="text-center">Filter by: </p>
+              {categories.map((category) => (
+                <button
+                  key={category}
+                  onClick={() => setSelectedCategory(category)}
+                  className={`${
+                    selectedCategory === category
+                      ? "bg-purple-400 text-gray-900"
+                      : "bg-gray-100 text-gray-900"
+                  } px-3 py-2 rounded-md text-sm font-medium`}
+                >
+                  {category}
+                </button>
+              ))}
+            </div>
+          </div>
         </div>
 
-        {services.map((service) => (
-          <div
-            key={service.id}
-            className="mx-auto max-w-2xl rounded-3xl ring-1 ring-gray-200 my-6 sm:my-8 lg:mx-0 lg:flex lg:max-w-none"
-          >
-            <div className="p-8 sm:p-10 lg:flex-auto">
-              <h3 className="text-2xl font-bold tracking-tight text-gray-900">
-                {service.serviceName}
-              </h3>
-              <p className="mt-6 text-base leading-7 text-gray-600">
-                Lorem ipsum dolor sit amet consect etur adipisicing elit. Itaque
-                amet indis perferendis blanditiis repellendus etur quidem
-                assumenda.
-              </p>
-              <div className="mt-10 flex items-center gap-x-4">
-                <h4 className="flex-none text-sm font-semibold leading-6 text-indigo-600">
-                  What’s included
-                </h4>
-                <div className="h-px flex-auto bg-gray-100" />
+        {services
+          .filter((service) => {
+            if (selectedCategory === "All") {
+              return true;
+            } else if (selectedCategory === "Men") {
+              return (
+                service.serviceName.includes("Men's") ||
+                service.serviceName.includes("Boy's")
+              );
+            } else if (selectedCategory === "Women") {
+              return (
+                service.serviceName.includes("Women's") ||
+                service.serviceName.includes("Girl's")
+              );
+            } else if (selectedCategory === "Children") {
+              return (
+                service.serviceName.includes("Boy's") ||
+                service.serviceName.includes("Girl's")
+              );
+            } else {
+              return false;
+            }
+          })
+          .map((service) => (
+            <div
+              key={service.id}
+              className="mx-auto max-w-2xl rounded-3xl ring-1 ring-gray-200 my-6 sm:my-8 lg:mx-0 lg:flex lg:max-w-none"
+            >
+              <div className="p-8 sm:p-10 lg:flex-auto">
+                <h3 className="text-2xl font-bold tracking-tight text-gray-900">
+                  {service.serviceName}
+                </h3>
+                <p className="mt-6 text-base leading-7 text-gray-600">
+                  {service.serviceDescription}
+                </p>
+                <div className="mt-10 flex items-center gap-x-4">
+                  <h4 className="flex-none text-sm font-semibold leading-6 text-indigo-600">
+                    What’s included
+                  </h4>
+                  <div className="h-px flex-auto bg-gray-100" />
+                </div>
+                <ul
+                  role="list"
+                  className="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6"
+                >
+                  {service.features.map((feature) => (
+                    <li key={feature} className="flex gap-x-3">
+                      <CheckIcon
+                        className="h-6 w-5 flex-none text-indigo-600"
+                        aria-hidden="true"
+                      />
+                      {feature}
+                    </li>
+                  ))}
+                </ul>
               </div>
-              <ul
-                role="list"
-                className="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6"
-              >
-                {service.features.map((feature) => (
-                  <li key={feature} className="flex gap-x-3">
-                    <CheckIcon
-                      className="h-6 w-5 flex-none text-indigo-600"
-                      aria-hidden="true"
-                    />
-                    {feature}
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
-              <div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
-                <div className="mx-auto max-w-xs px-8">
-                  <p className="text-base font-semibold text-gray-600">
-                    Book now, pay later
-                  </p>
-                  <p className="mt-6 flex items-baseline justify-center gap-x-2">
-                    <span className="text-5xl font-bold tracking-tight text-gray-900">
-                      ${service.price}
-                    </span>
-                    <span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">
-                      USD
-                    </span>
-                  </p>
-                  <a
-                    href="#"
-                    className="mt-8 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  >
-                    Book Now
-                  </a>
-                  <p className="mt-6 text-xs leading-5 text-gray-600">
-                    {service.time} minutes
-                  </p>
+              <div className="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
+                <div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
+                  <div className="mx-auto max-w-xs px-8">
+                    <p className="text-base font-semibold text-gray-600">
+                      Book now, pay later
+                    </p>
+                    <p className="mt-6 flex items-baseline justify-center gap-x-2">
+                      <span className="text-5xl font-bold tracking-tight text-gray-900">
+                        ${service.price}
+                      </span>
+                      <span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">
+                        USD
+                      </span>
+                    </p>
+                    <a
+                      href="#"
+                      className="mt-8 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                      Book Now
+                    </a>
+                    <p className="mt-6 text-xs leading-5 text-gray-600">
+                      {service.time} minutes
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
       </main>
     </div>
   );
